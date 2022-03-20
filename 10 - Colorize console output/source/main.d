d56f1e71d34cfbef8252f7ba770c4745a3659d3b
Guillaume Piolat <contact@auburnsounds.com>
2022-03-20 19:02:42 +0100

    Update DIID #10

--- 10 - Colorize console output/source/main.d
+++ 10 - Colorize console output/source/main.d
@@ -1,8 +1,7 @@
-import colorize;
+import consolecolors;
 
 void main()
 {
-    cwriteln("This is light_blue".color(fg.light_blue));
-    auto c = "light_red";
-    cwritefln("This is %s in this nested coloured text".color("red"), c.color(c));
+    cwriteln("This is light blue on orange".lblue.on_orange);    
+    cwritefln("This is a <red>%s</red>.", "nested <yellow>color</yellow> string");
 }
